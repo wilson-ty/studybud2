4e075eea0cc0ceb25238793dffa394b8f4dac354
wilson-ty <129957019+wilson-ty@users.noreply.github.com>
2023-05-26 12:06:53 +0300

    first commit

---
+++ base/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+rooms = [
+    {"id": 1, "name": "Lets Learn Python"},
+    {"id": 2, "name": "Design With Me"},
+    {"id": 3, "name": "Fronted Developers"},
+]
+
+
+def home(request):
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
+
+    return render(request, "base/room.html", context)
